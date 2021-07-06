eb8a1ff0a4b52bb2001c825dc50ea1e56b77ac41
Bapan2003 <85957141+Bapan2003@users.noreply.github.com>
2021-07-06 10:43:20 +0530

    Add files via upload

---
+++ pattern number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int i,j,a;
+printf("\n enter the value of a :");
+scanf("%d",&a);
+for(i=1;i<=a;i++){
+for(j=1;j<=i;j++)
+printf("%d ",i);
+printf("\n ");
+}
+return 0;
+}
